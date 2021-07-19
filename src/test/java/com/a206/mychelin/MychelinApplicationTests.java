96caf3f421a46e2e702e4da1e716db5023416897
Hailey Kim <haileykim@Haileyui-MacBookAir.local>
2021-07-19 16:00:06 +0900

    setting : initial project setting

---
+++ src/test/java/com/a206/mychelin/MychelinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a206.mychelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychelinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
