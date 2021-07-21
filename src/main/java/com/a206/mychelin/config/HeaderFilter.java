3695ea8380ec8a6b51db321b3084a03472139f75
haileyHi <yuhiro@naver.com>
2021-07-21 15:52:50 +0900

    add : HeaderFilter

---
+++ src/main/java/com/a206/mychelin/config/HeaderFilter.java
@@ -0,0 +1,31 @@
+package com.a206.mychelin.config;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+public class HeaderFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        final HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader(
+                "Access-Control-Allow-Headers",
+                "X-Requested-With, Content-Type, Authorization, X-XSRF-token"
+        );
+        res.setHeader(
+                "Access-Control-Expose-Headers",
+                "Refresh_token"
+        );
+        res.setHeader("Access-Control-Allow-Credentials", "false");
+
+        chain.doFilter(request, response);
+    }
+
+}
