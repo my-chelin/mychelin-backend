50722730cc3786d3ba2c719b6a85abe031aad0b6
정준영 <cua0918@naver.com>
2021-07-23 18:24:49 +0900

    fetch : 식당 검색 시 평점 추가

--- src/main/java/com/a206/mychelin/domain/repository/PlaceRepository.java
+++ src/main/java/com/a206/mychelin/domain/repository/PlaceRepository.java
@@ -2,6 +2,8 @@ package com.a206.mychelin.domain.repository;
 
 import com.a206.mychelin.domain.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +15,9 @@ public interface PlaceRepository extends JpaRepository<Place, Integer> {
     List<Place> findPlacesByNameContains(String name);
 
     List<Place> findPlacesByLocationContains(String location);
+
+    @Query(value = "SELECT AVG(e.star_rate) FROM review e WHERE e.place_id = ?1" , nativeQuery = true)
+    Optional<Double> getStartRateById(String id);
 
 
 
