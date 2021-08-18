b55e82085320226e1a8b49add9100c4faf9435a5
Yeong-a Jang <jya005@gmail.com>
2021-08-18 13:26:24 +0900

    fix: main feed token check

--- src/main/java/com/a206/mychelin/config/AuthorizationCheck.java
+++ src/main/java/com/a206/mychelin/config/AuthorizationCheck.java
@@ -17,6 +17,8 @@ public class AuthorizationCheck {
         tokenRequiredPathList.add("/follow/request");
         tokenRequiredPathList.add("/follow/requestList");
         tokenRequiredPathList.add("/post/upload");
+        tokenRequiredPathList.add("/post/main");
+        tokenRequiredPathList.add("/post/search");
         tokenRequiredPathList.add("/place/review");
         tokenRequiredPathList.add("/place/review/image/{reviewId}");
         tokenRequiredPathList.add("/placelist");
